11201759d3a87adb474b4461251bdab22383483d
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2021-03-31 11:52:39 -0600

    Add objectives

--- sketch_mar31a.ino
+++ sketch_mar31a.ino
@@ -1,3 +1,14 @@
+#OBJECTIVES
+
+#OB 1: Read in a simple 2 tone wave
+
+#OB 2: Play back a wave
+
+#OB 3: Implement a Fourier transform formula function. It takes a wave, and returns a fourier generated map of tones
+#OB 4: Override the Fourier function to take a second parameter, a list of indices, which dictates which tones should be filtered out. This returns the filtered frequency instead of a map.
+
+#OB 5: Implement function that takes two waves, sums them, and returns that sum
+
 void setup() {
   // put your setup code here, to run once:
 
